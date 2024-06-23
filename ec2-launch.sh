78447ac67d28222f66961b7b0585f83e4a8a2b32
ArpanaM <muriki.arpana@gmail.com>
2024-06-23 07:49:51 +0530

    Roboshop proj created

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+#aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=name,Value=frontend}]" | jq
+#
 
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=name,Value=frontend}]"
+TEMP_ID="ami-0f3c7d07486cad139"
+TEMP_VER=2
 
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}| jq
