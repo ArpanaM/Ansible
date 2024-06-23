14108fe61d70fff51a09df8c17b6e799b294a4d2
ArpanaM <muriki.arpana@gmail.com>
2024-06-23 17:29:28 +0530

    Roboshop proj created

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -4,10 +4,11 @@
 #
  #aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name="running" | sed 's/"//g'
 aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
-if [ $? -eq 0 ]; then
+if [ $? -eq -0 ]; then
   echo "instance already there"
-  else
+  exit
+  fi
 TEMP_ID="lt-07e05017fc2ec8348"
 TEMP_VER=5
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
-fi
+
