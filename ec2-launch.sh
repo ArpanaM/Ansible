15701313a38cf0d9669f856fb8a75aec4de40d13
ArpanaM <muriki.arpana@gmail.com>
2024-06-23 17:49:36 +0530

    Create an instance using Launch Template if instance exists then shows message as already exists and exits else it creates

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,15 +3,22 @@
 #aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=name,Value=frontend}]" | jq
 #
  #aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name="running" | sed 's/"//g'
+if [ -z "$1" ]; then
+  echo -e "\e[1;31mInput is missing\e[0m"
+  exit 1
+fi
+
+COMPONENT=$1  # first argument
 TEMP_ID="lt-07e05017fc2ec8348"
 TEMP_VER=5
 
-aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq -0 ]; then
-  echo "instance already there"
+  echo -e "\e[1;33minstance already there\e[0m"
   exit
   fi
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,
+Tags=[{Key=Name,Value=${COMPONENT}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]" | jq
 
 # Update the DNS Record
