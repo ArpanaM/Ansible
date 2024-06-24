abd477eee109e70fa622b361fc42d5c77b734b5a
ArpanaM <muriki.arpana@gmail.com>
2024-06-24 14:53:42 +0530

    Create an instance using Launch Template if instance exists then shows message as already exists and exits else it creates

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -14,22 +14,18 @@ TEMP_VER=5
 ZONE_ID=Z04776882HVJTCOQ8IAU3
 
 
-
-
-
-
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq -0 ]; then
   echo -e "\e[1;33minstance already there\e[0m"
-  exit
+else
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
   fi
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,
-Tags=[{Key=Name,Value=${COMPONENT}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]" | jq
 
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
+# Update the DNS Record
 sed -e "s/IPADDRESS/${IPADDRESS}" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
-
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file://tmp/record.json | jq
 
-# Update the DNS Record
+
